f632f2858610d9e22519bf67831ade2b962a3740
ayodeji oni <ayodejioni1505@gmail.com>
2025-04-18 19:47:12 +0100

    add oop implementation

---
+++ main.py
@@ -0,0 +1,18 @@
+from pet import Pet
+import time
+
+time.sleep(0.5)
+animal = Pet("Rex")
+time.sleep(0.8)
+animal.eat()
+time.sleep(0.8)
+animal.play()
+time.sleep(0.8)
+animal.sleep()
+time.sleep(0.8)
+animal.train("roll over")
+time.sleep(0.8)
+animal.train("play dead")
+time.sleep(0.8)
+print()
+animal.get_status()
---
+++ pet.py
@@ -0,0 +1,36 @@
+class Pet:
+    def __init__(self, name):
+        self.name = name
+        self.hunger = 5
+        self.energy = 5
+        self.happiness = 5
+        self.tricks = []
+        print(f"Creating pet: {self.name}")
+
+    def eat(self):
+        self.hunger = max(0, self.hunger - 3)
+        self.happiness += 1
+        print(f"{self.name} is eating...")
+
+    def sleep(self):
+        self.energy = min(self.energy + 5, 10)
+        print(f"{self.name} is sleeping...")
+
+    def play(self):
+        self.energy -= 2
+        self.happiness += 1
+        self.hunger += 1
+        print(f"{self.name} is playing...")
+
+    def train(self, trick):
+        self.tricks.append(trick)
+
+    def show_tricks(self):
+        return self.tricks
+
+    def get_status(self):
+        print(f"""{self.name}'s current status:
+Hunger: {self.hunger}
+Energy: {self.energy}
+Happiness: {self.happiness}
+Tricks: {Pet.show_tricks(self)}""")
